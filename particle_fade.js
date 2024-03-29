aaece40816373357cf22f08b21aa4912a22238dd
KennethMinn <minthukha2472005@gmail.com>
2024-03-29 10:18:31 +0630

    constellation effect

---
+++ particle_fade.js
@@ -0,0 +1,63 @@
+const canvas = document.getElementById("canvas1");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(canvas.width, canvas.height);
+let particlesArray = [];
+
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+
+class Particle {
+  constructor() {
+    // this.x = mouse.x;
+    // this.y = mouse.x;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 15 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.size > 0.2) {
+      this.size -= 0.01;
+    }
+  }
+
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+(function () {
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
+})();
+
+console.log(particlesArray);
+
+const handleParticles = () => {
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+  }
+};
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  requestAnimationFrame(animate);
+}
+
+animate();
